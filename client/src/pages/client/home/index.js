80e62f4578fcadf4e7e6767b2e40f5bcf7acb682
Victor Moak <vmoak10@gmail.com>
2021-07-26 10:53:57 -0300

    depoloy

--- client/src/pages/client/home/index.js
+++ client/src/pages/client/home/index.js
@@ -3,7 +3,8 @@ import React from 'react';
 export default function Home() {
     return (
         <div>
-            <meta http-equiv="Refresh" content="0; url=../../admin/" /> 
+            <h1>HOme</h1>
+            {/* <meta http-equiv="Refresh" content="0; url=../../admin/" />  */}
             {/*Redirecionamento temporario. Até vincular com a página cliente*/}
         </div>
     );
